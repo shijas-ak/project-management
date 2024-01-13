c069de2af1a8b982690a56af8af86bcf1e1dac52
shijas-ak <shijas@nuox.io>
2024-01-13 14:56:54 +0530

    mongoose error

--- server/app.js
+++ server/app.js
@@ -1,34 +1,30 @@
-const express = require('express')
+const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
-const cors = require('cors')
-const authRoutes = require('./src/routes/authRoutes')
-const projectRoutes = require('./src/routes/projectRoutes')
-const taskRoutes = require('./src/routes/taskRoutes')
-const assignRoutes = require('./src/routes/assignRoutes')
-const forgotPasswordRoute = require('./src/password-reset/forgotPassword')
-const resetPasswordRoute = require('./src/password-reset/resetPassword')
-const profileRoutes = require('./src/routes/profileRoutes')
+const cors = require("cors");
+const authRoutes = require("./src/routes/authRoutes");
+const projectRoutes = require("./src/routes/projectRoutes");
+const taskRoutes = require("./src/routes/taskRoutes");
+const assignRoutes = require("./src/routes/assignRoutes");
+const forgotPasswordRoute = require("./src/password-reset/forgotPassword");
+const resetPasswordRoute = require("./src/password-reset/resetPassword");
+const profileRoutes = require("./src/routes/profileRoutes");
 
-
-
-
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({ extended: true}))
-app.use(cors())
-app.use('/images', express.static('./public/images'));
-app.use(authRoutes)
-app.use('/projects',projectRoutes)
-app.use(taskRoutes)
-app.use(assignRoutes)
-app.use(forgotPasswordRoute)
-app.use(resetPasswordRoute)
-app.use('/users-profile',profileRoutes)
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use("/images", express.static("./public/images"));
+app.use(authRoutes);
+app.use("/projects", projectRoutes);
+app.use(taskRoutes);
+app.use(assignRoutes);
+app.use(forgotPasswordRoute);
+app.use(resetPasswordRoute);
+app.use("/users-profile", profileRoutes);
 
 const PORT = process.env.PORT;
-const mongoUrl = process.env.MONGO_URL;
-
+const mongoUrl = process.env.MONGO_URL || "mongodb://127.0.0.1:27017";
 
 mongoose
   .connect(mongoUrl)
@@ -39,4 +35,3 @@ mongoose
     });
   })
   .catch((error) => console.log(error));
-  
